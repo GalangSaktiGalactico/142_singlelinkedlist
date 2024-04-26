d92ed1df2608ecd9f1884c1a101ce3e84766df27
Galang Sakti G <wadepakmen97@gmail.com>
2024-04-26 11:27:06 +0700

    case 3-5

--- SingleLinkedList/SingleLinkedList.cpp
+++ SingleLinkedList/SingleLinkedList.cpp
@@ -168,3 +168,11 @@ int main() {
 				else
 					cout << "Data tidak ditemukan" << endl;
 				break;
+			case 3:
+				traverse();
+				break;
+			case 4:
+				searchData();
+				break;
+			case 5:
+				break;

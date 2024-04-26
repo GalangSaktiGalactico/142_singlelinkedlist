d6244d97c751268b0a89ce1aa809fefd39011c68
Galang Sakti G <wadepakmen97@gmail.com>
2024-04-26 11:27:38 +0700

    last commit

--- SingleLinkedList/SingleLinkedList.cpp
+++ SingleLinkedList/SingleLinkedList.cpp
@@ -176,3 +176,15 @@ int main() {
 				break;
 			case 5:
 				break;
+			default:
+				cout << "Pilihan tidak ada" << endl;
+				break;
+			}
+		}
+		catch (exception e)
+		{
+			cout << "Terjadi Kesalahan" << endl;
+		}
+
+	} while (pilihan != 5);
+}
